efd52dc3161b98720f1e5b201b66f7294a1d6411
agentden <den.bat@mail.ru>
2015-12-06 00:56:48 +0500

    test commit
    test

--- _admin/index.php
+++ _admin/index.php
@@ -9,24 +9,36 @@ session_start();
 <!DOCTYPE html>
 <html lang="ru">
 <head>	
-	<link rel="stylesheet" href="css/admin.css">
+	<link rel="stylesheet" href="../css/admin.css">
 	<meta charset="utf-8">
 
-	<title>Админка</title>
+	<title>Вход в систему</title>
 </head>
 <body>
 	<section>
+	<H1>Вход в систему</H1>
+	<p><b>Для доступа к панели управления сайтом, введите:</b></p>
 		<form action="test.php" method="post">
-			<label><p>Логин: 
-				<input type="text" name="login" value="Ваш логин">
-				</p>
-			</label>
-			<label><p>Пароль: 
-					<input type="password" name="password" value="">
-				</p>
-			</label>
-			<input type="submit" value="Отправить">
+			<table>				
+				<tr>
+					<label>	
+					<td>Логин:</td>
+					<td><input type="text" name="login" value=""></td>	
+					</label>				
+				</tr>			
+				<tr>
+					<label>
+					<td>Пароль:</td>
+					<td><input type="password" name="password" value=""></td>
+					</label>					
+				</tr>
+				<tr>
+					<td id="button" colspan="2"><input type="submit" value="Войти в систему" class="inp_but"></td>										
+				</tr>			
+			</table>
 		</form>
+		<hr>
+		<p>CMS &copy; Liberty</p>
 	</section>
 		<footer>			
 		</footer>
